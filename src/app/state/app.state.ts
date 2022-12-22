5a5d70b91b33e98a3dec062765e79807fab94d62
David Rodrigues <davidrodrigues@quickenloans.com>
2022-12-22 14:13:52 -0500

    more formating

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -4,9 +4,3 @@ export interface AppState {
   books: ReadonlyArray<Book>;
   collection: ReadonlyArray<string>;
 }
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
