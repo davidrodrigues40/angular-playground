b1d6aab9f6e856af0d6a40cf9bb5a372fa8bca14
David Rodrigues <davidrodrigues@quickenloans.com>
2022-12-22 14:11:29 -0500

    more formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,4 @@ import { BookCollectionComponent } from './book-collection/book-collection.compo
   declarations: [AppComponent, BookListComponent, BookCollectionComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
+export class AppModule { }
--- src/app/book-collection/book-collection.component.ts
+++ src/app/book-collection/book-collection.component.ts
@@ -4,15 +4,9 @@ import { Book } from '../book-list/books.model';
 @Component({
   selector: 'app-book-collection',
   templateUrl: './book-collection.component.html',
-  styleUrls: ['./book-collection.component.css'],
+  styleUrls: ['./book-collection.component.scss'],
 })
 export class BookCollectionComponent {
   @Input() books: ReadonlyArray<Book> = [];
   @Output() remove = new EventEmitter<string>();
 }
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
